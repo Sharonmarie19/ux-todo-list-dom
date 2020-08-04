e1b7d2d327de5032ea55a29e666f9aa6bf3b939f
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-08-04 15:17:35 -0400

    I'm trying

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  document.body.append(h1);
 
-    h1.innerText = "Todo List"
+for (let i = 0; i< todo.length; i++){
+    const todoList = todos[i]
+    let list = `
+    <li class='todo-item'>
+        <p>${todoList.completed}</p>
 
-    app.appendChild(h1)
-    // and maybe some here
+        <p>${todoList.description}</p>
+    </li>
+    `
+
+  h1.innerText = "Todo List";
+
+  document.app.appendChild(h1);
+  <h1>Todo List</h1>
+  
 }
 
-renderTodoApp()
+renderTodoApp();
