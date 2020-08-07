907464476194386ded92c95343d45de52d175ac0
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-08-07 14:46:16 -0400

    trying

--- src/index.js
+++ src/index.js
@@ -21,21 +21,29 @@ function renderTodoApp() {
   let app = document.querySelector("#app");
   let h1 = document.createElement("h1");
   let todoList = document.createElement("ul");
+  let todoText = document.querySelector("#todo-text");
+  let todoButton = document.querySelector("#todo-button");
+
   h1.innerText = "Todo List";
+
   app.appendChild(h1);
   app.appendChild(todoList);
 
   for (let i = 0; i < todos.length; i++) {
     let todoItems = todos[i];
-     let stateOfBox = todoItems.completed ? "checked": "";
-        let todoMarkup = `
+    let stateOfBox = todoItems.completed ? "checked" : "";
+    let todoMarkup = `
     <li><input type="checkbox" ${stateOfBox}/>${todoItems.description}
     </li>
 
     `;
     todoList.innerHTML += todoMarkup;
-
   }
 }
 
 renderTodoApp();
+
+function addTodo(event) {
+  console.log("hello");
+}
+todoButton.addEventListener("click", addTodo);
